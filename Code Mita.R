eb888ad80717f16310edf87aba0845cc85181246
Tancrède Polge <tancrede.polge@gmail.com>
2020-11-18 00:42:12 +0100

    no message

--- Code Mita.R
+++ Code Mita.R
@@ -47,6 +47,7 @@ reg_1c <- lm(lhhequiv ~ pothuan_mita
 reg_1c_cse <- coeftest(reg_1c,vcovCL, cluster = mitaData_50$district)[,2]
 
 stargazer(reg_1a, reg_1b, reg_1c, se = list(reg_1a_cse, reg_1b_cse, reg_1c_cse), keep = "pothuan_mita", type = "text")
+#stargazer(reg_1a, reg_1b, reg_1c, se = list(reg_1a_cse, reg_1b_cse, reg_1c_cse), keep = "pothuan_mita", type = "latex", out ="/Users/tancredepolge/Documents/M2/M2S1/Political & Organizational/Replication/Tex/reg1.tex")
 
     ## Question D ##
 
@@ -81,3 +82,39 @@ reg_2c <- lm(lhhequiv ~ pothuan_mita
 reg_2c_cse <- coeftest(reg_2c,vcovCL, cluster = mitaData_50$district)[,2]
 
 stargazer(reg_2a, reg_2b, reg_2c, se = list(reg_2a_cse, reg_2b_cse, reg_2c_cse), keep = "pothuan_mita",type = "text") 
+#stargazer(reg_2a, reg_2b, reg_2c, se = list(reg_2a_cse, reg_2b_cse, reg_2c_cse), keep = "pothuan_mita", type = "latex", out ="/Users/tancredepolge/Documents/M2/M2S1/Political & Organizational/Replication/Tex/reg2.tex")
+
+    ## Question F ##
+
+mitaData_short <- aggregate(mitaData, by = list(mitaData$district), FUN = "mean")
+#View(mitaData_short)
+
+# Constructing the distance to Potosi polynomial terms
+mitaData_short$dpot2 <- mitaData_short$dpot^2
+mitaData_short$dpot3 <- mitaData_short$dpot^3
+
+#  Re-constructing samples according to distances to mita boundaries (with the new variables dpot^2 and dpot^3)
+mitaData_short_100 <- filter(mitaData_short, d_bnd < 100)
+mitaData_short_75 <- filter(mitaData_short, d_bnd < 75)
+mitaData_short_50 <- filter(mitaData_short, d_bnd < 50)
+
+# Regression with distance to boundary < 100 km 
+reg_3a <- lm(lhhequiv ~ pothuan_mita 
+             + dpot + dpot2 + dpot3 
+             + elv_sh + slope + infants + children + adults + bfe4_1 + bfe4_2 + bfe4_3, 
+             data = mitaData_short_100)
+
+# Regression with distance to boundary < 75 km 
+reg_3b <- lm(lhhequiv ~ pothuan_mita 
+             + dpot + dpot2 + dpot3 
+             + elv_sh + slope + infants + children + adults + bfe4_1 + bfe4_2 + bfe4_3, 
+             data = mitaData_short_75)
+
+# Regression with distance to boundary < 50 km 
+reg_3c <- lm(lhhequiv ~ pothuan_mita 
+             + dpot + dpot2 + dpot3 
+             + elv_sh + slope + infants + children + adults + bfe4_1 + bfe4_2 + bfe4_3, 
+             data = mitaData_short_50)
+
+stargazer(reg_3a, reg_3b, reg_3c, keep = "pothuan_mita",type = "text")
+#stargazer(reg_3a, reg_3b, reg_3c, keep = "pothuan_mita", type = "latex", out ="/Users/tancredepolge/Documents/M2/M2S1/Political & Organizational/Replication/Tex/reg3.tex")
